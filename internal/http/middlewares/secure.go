d99f550f31b5da63f014626072034a13662a2058
sujit <s.baniya.np@gmail.com>
2024-07-24 16:55:23 +0545

    feat: Initial Commit

--- internal/http/middlewares/secure.go
+++ internal/http/middlewares/secure.go
@@ -20,7 +20,7 @@ func SecureHeaders() fiber.Handler {
 		}
 
 		// Content Security Policy: Define loading policy for all resources type
-		c.Set("Content-Security-Policy", "default-src gap://ready file://* *; style-src 'self' http://* https://* 'unsafe-inline'; script-src 'self' http://* https://* 'unsafe-inline' 'unsafe-eval'")
+		c.Set("Content-Security-Policy", "default-src gap://ready file://* *; img-src 'self' http://* https://* data:; style-src 'self' http://* https://* 'unsafe-inline'; script-src 'self' http://* https://* 'unsafe-inline' 'unsafe-eval'")
 
 		// Prevent MIME type sniffing vulnerabilities
 		c.Set("X-Content-Type-Options", "nosniff")
