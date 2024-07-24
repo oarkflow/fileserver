10a99c369ee4ff09c9d6575990b29112d476e5a4
sujit <s.baniya.np@gmail.com>
2024-07-24 17:02:11 +0545

    feat: Initial Commit

--- internal/routes/web.go
+++ internal/routes/web.go
@@ -1,8 +1,12 @@
 package routes
 
 import (
+	"path/filepath"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 
+	"boxen_dispatch/internal/entities"
 	"boxen_dispatch/internal/http/controllers"
 )
 
@@ -10,6 +14,7 @@ func Web(app fiber.Router, env string) {
 	if env == "test" {
 		app.Get("/test-panic", controllers.Home.Test)
 	}
+	app.Use(redirectInvalidDir)
 	// Unprotected routes
 	LandingRoutes(app)
 }
@@ -19,4 +24,25 @@ func LandingRoutes(app fiber.Router) {
 	app.Get("/ping", controllers.Home.Ping)
 	app.Get("/view", controllers.Home.View)
 	app.Get("/get", controllers.Home.Get)
+}
+
+func redirectInvalidDir(c *fiber.Ctx) error {
+	dir := c.Query("dir")
+	if dir != "" && !checkPath(entities.Workspace.Paths(), dir) {
+		return c.Redirect("/")
+	}
+	return c.Next()
+}
+
+func checkPath(baseDirs []string, path string) bool {
+	path = filepath.Clean(path)
+	for _, baseDir := range baseDirs {
+		baseDir = filepath.Clean(baseDir)
+		if strings.HasPrefix(path, baseDir) {
+			if relPath, err := filepath.Rel(baseDir, path); err == nil && !strings.Contains(relPath, "..") {
+				return true
+			}
+		}
+	}
+	return false
 }
